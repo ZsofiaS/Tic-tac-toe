0f75d26579fe6bbfe065ad97ca78575187b4b2ec
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-10-21 15:47:15 +0100

    Add squares to board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Board = styled.div`
-    width: 600px;
-    height: 600px;
+const BoardContainer = styled.div`
+    width: 630px;
+    height: 630px;
     padding: 1rem;
     border: 1px solid gainsboro;
     margin: 2rem auto;
-`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+`;
+
+const Square = styled.button`
+    width: 200px;
+    height: 200px;
+    border: 1px solid crimson;
+    margin: 4px;
+`;
+
+const Board = () => {
+    return (
+        <BoardContainer>
+            <Square value="1" onClick={(e) => console.log(e.target.value)}></Square>
+            <Square value="2" onClick={(e) => console.log(e.target.value)}></Square>
+            <Square value="3" onClick={(e) => console.log(e.target.value)}></Square>
+            <Square value="4" onClick={(e) => console.log(e.target.value)}></Square>
+            <Square value="5" onClick={(e) => console.log(e.target.value)}></Square>
+            <Square value="6" onClick={(e) => console.log(e.target.value)}></Square>
+            <Square value="7" onClick={(e) => console.log(e.target.value)}></Square>
+            <Square value="8" onClick={(e) => console.log(e.target.value)}></Square>
+            <Square value="9" onClick={(e) => console.log(e.target.value)}></Square>
+        </BoardContainer>
+    )
+}
 
 export default Board;
--- src/tests/Board.test.js
+++ src/tests/Board.test.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import Board from '../components/Board';
+import Square from '../components/Board';
 
-describe('renders Board components', () => {
+describe('renders Square components', () => {
     it('renders without crashing', () => {
-        shallow(<Board />);
+        shallow(<Square />);
     });
 });
