f39adb191b8338f61c543b30a7369d4b944a5f87
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-10-22 12:21:19 +0100

    Add more unit tests to Board component

--- src/tests/Board.test.js
+++ src/tests/Board.test.js
@@ -2,13 +2,25 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import { Board, Square } from '../components/Board';
+import Button from '../components/Button';
+
+let boardComponent;
+
+beforeEach(() => {
+  boardComponent = shallow(<Board />);
+});
 
 describe('renders Board component', () => {
   it('renders without crashing', () => {
     shallow(<Board />);
   });
+  it('should render 9 squares', () => {
+    expect(boardComponent.find(Square)).toHaveLength(9);
+  });
+  it('should render 1 button', () => {
+    expect(boardComponent.find(Button)).toHaveLength(1);
+  });
   it('renders Board correctly', () => {
-    const boardComponent = shallow(<Board />);
     expect(toJson(boardComponent)).toMatchSnapshot();
   });
 });
