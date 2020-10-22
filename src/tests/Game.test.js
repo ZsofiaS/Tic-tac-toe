9c81a9ad83149d712a05155db99c15624fd68b9f
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-10-22 12:27:11 +0100

    Add more unit tests to Game component

--- src/tests/Game.test.js
+++ src/tests/Game.test.js
@@ -2,13 +2,25 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import Game from '../components/Game';
+import { Board } from '../components/Board';
+
+let gameComponent;
+
+beforeEach(() => {
+  gameComponent = shallow(<Game />);
+});
 
 describe('renders Game component', () => {
   it('renders without crashing', () => {
     shallow(<Game />);
   });
+  it('should render a p element', () => {
+    expect(gameComponent.find('p')).toHaveLength(1);
+  });
+  it('should render a board component', () => {
+    expect(gameComponent.find(Board)).toHaveLength(1);
+  });
   it('renders Game correctly', () => {
-    const gameComponent = shallow(<Game />);
     expect(toJson(gameComponent)).toMatchSnapshot();
   });
 });
