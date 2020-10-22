c6fd905ee4cf61ed3d928be6b9880e93bc9f5086
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-10-22 12:29:51 +0100

    Add more unit tests to App component

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,13 +2,28 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import App from '../components/App';
+import Game from '../components/Game';
+
+let appComponent;
+
+beforeEach(() => {
+  appComponent = shallow(<App />);
+});
 
 describe('renders App component', () => {
   it('renders without crashing', () => {
     shallow(<App />);
   });
+  it('should render a h1 element', () => {
+    expect(appComponent.find('h1')).toHaveLength(1);
+  });
+  it('should render a p element', () => {
+    expect(appComponent.find('p')).toHaveLength(1);
+  });
+  it('should render a Game component', () => {
+    expect(appComponent.find(Game)).toHaveLength(1);
+  });
   it('renders App correctly', () => {
-    const appComponent = shallow(<App />);
     expect(toJson(appComponent)).toMatchSnapshot();
   });
 });
